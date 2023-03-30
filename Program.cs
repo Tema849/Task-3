9943bf49fd256e70aa79f7a808c3b8eeb029ccd6
Tema849 <qwaqwatern@gmail.com>
2023-03-30 20:01:19 +0300

    Задача 23

--- Program.cs
+++ Program.cs
@@ -39,22 +39,45 @@
 
 // Task:
 
-int x1 = ReadInt("Введите координату X первой точки: ");
-int y1 = ReadInt("Введите координату Y первой точки: ");
-int z1 = ReadInt("Введите координату Z первой точки: ");
-int x2 = ReadInt("Введите координату X второй точки: ");
-int y2 = ReadInt("Введите координату Y второй точки: ");
-int z2 = ReadInt("Введите координату Z второй точки: ");
+//int x1 = ReadInt("Введите координату X первой точки: ");
+//int y1 = ReadInt("Введите координату Y первой точки: ");
+//int z1 = ReadInt("Введите координату Z первой точки: ");
+//int x2 = ReadInt("Введите координату X второй точки: ");
+//int y2 = ReadInt("Введите координату Y второй точки: ");
+//int z2 = ReadInt("Введите координату Z второй точки: ");
+//
+//int A = x2 - x1;
+//int B = y2 - y1;
+//int C = z1 - z2;
 
-int A = x2 - x1;
-int B = y2 - y1;
-int C = z1 - z2;
+//double length = Math.Sqrt(A * A + B * B + C * C);
+//Console.WriteLine($"Длинна отрезка {length}");
+//
+//int ReadInt(string message)
+//{
+//    Console.Write(message);
+//    return Convert.ToInt32(Console.ReadLine());
+//}
 
-double length = Math.Sqrt(A * A + B * B + C * C);
-Console.WriteLine($"Длинна отрезка {length}");
+
+// Задача 23.
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// Пример:
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+// Task:
+
+int number = ReadInt("Введите число: ");
+
+for (int i = 1; i <= number; i++)
+{ 
+ Console.Write($"{i*i*i} ");
+}
 
 int ReadInt(string message)
 {
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
+ Console.Write(message);
+ return Convert.ToInt32(Console.ReadLine());
 }
